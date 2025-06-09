71e805f6fa326e0caa88f86dc0e3eb79c90f203b
matheus alves <matheus.alvesbarroso@gmail.com>
2025-06-08 21:49:55 -0300

    feat: file_handler tests

---
+++ cmd/dbpopulate/file_handler.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"b3challenge/internal/domain/entity"
+	"context"
+	"encoding/csv"
+	"io"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/shopspring/decimal"
+)
+
+const (
+	minRecordLength     = 10
+	minHourLength       = 6
+	tickerColumnIndex   = 1
+	priceColumnIndex    = 3
+	quantityColumnIndex = 4
+	hourColumnIndex     = 5
+	dateColumnIndex     = 8
+)
+
+func findTXTFiles(pathDir string) ([]string, error) {
+	entries, err := os.ReadDir(pathDir)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Error reading directory %s", pathDir)
+	}
+	var list []string
+	for _, e := range entries {
+		if !e.IsDir() && strings.HasSuffix(e.Name(), ".txt") {
+			path := filepath.Join(pathDir, e.Name())
+			list = append(list, path)
+		}
+	}
+
+	return list, nil
+}
+
+func parseTradeToEntity(rows []string) (*entity.Trade, error) {
+	if len(rows) < minRecordLength {
+		return nil, errors.New("invalid record length")
+	}
+
+	ticker := rows[tickerColumnIndex]
+	rawPrice := strings.ReplaceAll(rows[priceColumnIndex], ",", ".")
+	rawQty := rows[quantityColumnIndex]
+	rawHour := rows[hourColumnIndex]
+	rawDate := rows[dateColumnIndex]
+
+	price, err := decimal.NewFromString(rawPrice)
+	if err != nil {
+		return nil, errors.Wrap(err, "parsing price")
+	}
+	qty, err := strconv.ParseInt(rawQty, 10, 32)
+	if err != nil {
+		return nil, errors.Wrap(err, "parsing quantity")
+	}
+
+	hourPart := rawHour
+	if len(rawHour) >= minHourLength {
+		hourPart = rawHour[:minHourLength]
+	}
+
+	date, err := time.Parse(time.DateOnly, rawDate)
+	if err != nil {
+		return nil, errors.Wrap(err, "parsing date")
+	}
+
+	return entity.NewTrade(ticker, hourPart, date, price, int32(qty)), nil
+}
+
+func parseFileToTrades(ctx context.Context, filePath string, out chan<- entity.Trade) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return errors.Wrap(err, "cannot open file")
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.Comma = ';'
+
+	if _, err := reader.Read(); err != nil {
+		return errors.Wrap(err, "reading header")
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			slog.Info("Context cancelled, stopping file parsing")
+
+			return errors.Wrap(ctx.Err(), "context cancelled")
+
+		default:
+			rec, err := reader.Read()
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+
+			if err != nil {
+				slog.Error("CSV read error: ", slog.Any("error", err))
+
+				continue
+			}
+			trade, err := parseTradeToEntity(rec)
+			if err != nil {
+				slog.Error("parsing trade: ", slog.Any("error", err))
+
+				continue
+			}
+			out <- *trade
+		}
+	}
+}
---
+++ cmd/dbpopulate/file_handler_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"b3challenge/internal/domain/entity"
+	"context"
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+	"time"
+)
+
+func readFileTest(t *testing.T, file string) []byte {
+	b, err := os.ReadFile(file)
+	assert.NoError(t, err)
+
+	return b
+}
+
+func TestFindTXTFiles(t *testing.T) {
+	input := "testdata"
+	files, err := findTXTFiles(input)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(files))
+	assert.Equal(t, "testdata/mock-csv.txt", files[0])
+}
+
+func TestParseFileToTrades(t *testing.T) {
+	expectedTrades := []entity.Trade{
+		{
+			Ticker:   "TF583R",
+			Price:    decimal.New(10000, -3),
+			Quantity: 10000,
+			Hour:     "030507",
+			Date:     time.Date(2025, 6, 2, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			Ticker:   "FRCQ25",
+			Price:    decimal.New(5740, -3),
+			Quantity: 870,
+			Hour:     "090000",
+			Date:     time.Date(2025, 6, 2, 0, 0, 0, 0, time.UTC),
+		},
+	}
+
+	out := make(chan entity.Trade, 2)
+	gotErr := parseFileToTrades(context.Background(), "testdata/mock-csv.txt", out)
+	assert.NoError(t, gotErr)
+	close(out)
+
+	for _, want := range expectedTrades {
+		got, ok := <-out
+		assert.True(t, ok)
+		assert.Equal(t, want.Ticker, got.Ticker)
+		assert.Equal(t, want.Price, got.Price)
+		assert.Equal(t, want.Quantity, got.Quantity)
+		assert.Equal(t, want.Hour, got.Hour)
+		assert.Equal(t, want.Date, got.Date)
+	}
+}
